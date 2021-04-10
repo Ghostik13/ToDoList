7e0c688435417d4294c5adee9d1f7c5ee97bb32b
Ghostik13 <roxan95@mail.ru>
2021-04-11 00:51:33 +0300

    Updated to MVP with Moxy and Hilt

--- app/src/main/java/com/example/todolist/di/AppModule.kt
+++ app/src/main/java/com/example/todolist/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.example.todolist.domain.TaskDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+
 @Module
 @InstallIn(ToDoApplication::class)
 object AppModule {
